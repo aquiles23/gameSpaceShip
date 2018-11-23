32b74dc84f651a3286dd7bcbcfdc1ff64ba2ed7d
Rossicler Junior <hellslimm@hotmail.com>
2018-11-23 10:50:38 -0200

    Created AlienAgente

---
+++ src/AlienAgente.java
@@ -0,0 +1,8 @@
+
+public class AlienAgente {
+	// Apertar as setas <- e -> fazem com que ele também se movimente para o lado,
+	// porém se movimentando mais devagar que o usuário
+	// Após bater nas paredes laterais irá reagir andando para o lado oposto
+	
+	
+}
